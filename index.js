d9cbbe9afe2f566148a70dd1e5917fa99d0ac43c
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-04-04 08:16:28 +0500

    commands

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ import "dotenv/config";
 
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  const firstName = msg.from.first_name || "Foydalanuvchi";
-  bot.sendMessage(msg.chat.id, `Assalomu alaykum, ${firstName}!`);
+bot.setMyCommands([
+  {
+    command: "/start",
+    description: "Lounch the bot",
+  },
+  {
+    command: "/info",
+    description: "Learn about us",
+  },
+]);
+
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  if (text === "/start") {
+    return bot.sendMessage(
+      chatId,
+      "Assalom Alaykum office bot ga xush kelibsiz"
+    );
+  }
+
+  if (text === "/info") {
+    return bot.sendMessage(chatId, "Bizning botimiz haqida ma'lumot");
+  }
 });
